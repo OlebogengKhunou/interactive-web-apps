8c677fec47bcc2b855b166a4623cab9717fe929c
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-03-27 17:09:55 +0200

    initial commit

--- SRC/JAVA/script.js
+++ SRC/JAVA/script.js
@@ -1,33 +1,33 @@
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
-const STEP_AMOUNT = 1;
+const STEP_AMOUNT = 1
 
 const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="number"]');
-const add = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
 
-const subtracthandler = () => {
+const subtractHandler = () => {
     const newValue = parseInt(number.value) - STEP_AMOUNT
     number.value = newValue
-    
-    if (add.disabled === true){
-        add.disabled = false;
+
+    if (add.disabled === true) {
+        add.disabled = false
     }
     if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true;
-    } 
+        subtract.disabled = true
+    }
 
 }
-const addhandler = () => {
+const addHandler = () => {
     const newValue = parseInt(number.value) + STEP_AMOUNT
     number.value = newValue
 
     if (subtract.disabled === true) {
-        subtract.disabled = false;
+        subtract.disabled = false
     }
     if (newValue >= MAX_NUMBER) {
-        add.disabled = true;
-    } 
+        add.disabled = true
+    }
 }
-subtract.addEventListener('click', subtracthandler)
-add.addEventListener('click', addhandler)
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
