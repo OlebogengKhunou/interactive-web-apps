ecf6a7c9da1bff2cd3a79db867a5720d19c435bd
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-03-29 13:47:03 +0200

    code debugged

--- IWA3/challenge2/alex.js
+++ IWA3/challenge2/alex.js
@@ -1,6 +1,6 @@
 const firstname = "Alex"
 const surname = "Bloom"
-export let role = "Head of Marketing "
+export let role = "Head of Marketing"
 
-const display= firstname + " " + surname + " (" + role + ")"
+const display = firstname + " " + surname + "(" + role + ")"
 document.querySelector('#alex').innerText = display
--- IWA3/challenge2/nwabisa.js
+++ IWA3/challenge2/nwabisa.js
@@ -2,5 +2,5 @@ const firstname = "Nwabisa"
 const surname = "Gabe"
 export let role = "CEO"
 
-const display= firstname +" "+ surname + " (" + role + ") "
+const display = firstname + " " + surname + "(" + role + ")"
 document.querySelector('#nwabisa').innerText = display
